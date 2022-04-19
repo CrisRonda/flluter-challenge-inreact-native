3dcdd0cdf97335b767dc6ddfd12cdef13f9a7994
CrisRondaS <92877715+CrisRondaS@users.noreply.github.com>
2022-04-18 21:38:42 -0500

    installed reanimated v2

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import { View, Text, FlatList } from 'react-native';
+import {
+    useAnimatedStyle,
+    useSharedValue,
+    withRepeat,
+    withTiming
+} from 'react-native-reanimated';
 
 import { bannerRef, useSlider } from '../../context/SliderContext';
 import PlaceCard from './PlaceCard';
@@ -8,6 +14,12 @@ import styles from './styles';
 
 const Banner = () => {
     const { places } = useSlider();
+    const translateX = useSharedValue(0);
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: translateX.value }]
+        };
+    });
 
     return (
         <View style={styles.root}>
